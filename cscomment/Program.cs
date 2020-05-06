03be6d2871b90ecfe41db3a2ccbe8ddacfc7266d
연우 이 <lyw9114@gmail.com>
2020-05-06 14:06:07 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
